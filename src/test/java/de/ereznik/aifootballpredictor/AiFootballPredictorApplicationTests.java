fe718cd60a877340883b6f57904fc5e08bc8a7e0
Evgenij Reznik <evgenij.reznik@gmx.de>
2026-04-12 17:57:19 +0200

    Added local profile for test

--- src/test/java/de/ereznik/aifootballpredictor/AiFootballPredictorApplicationTests.java
+++ src/test/java/de/ereznik/aifootballpredictor/AiFootballPredictorApplicationTests.java
@@ -2,12 +2,13 @@ package de.ereznik.aifootballpredictor;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class AiFootballPredictorApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
